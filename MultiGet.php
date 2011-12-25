e4ba2843aa09d7dedba4c4c67d880a0b7698a382
Yaffle <vic99999@yandex.ru>
2011-12-25 13:31:22 +0600

    setTimeout

--- MultiGet.php
+++ MultiGet.php
@@ -59,6 +59,9 @@ class MultiGet {
 
     private $requests;
     private $queue = array();// order of loading is not garanted
+    
+
+    private $timeouts = array();
 
     public function __construct($maxRequests = 4, $curlOptions = null) {
         $this->maxRequests = $maxRequests;
@@ -82,6 +85,10 @@ class MultiGet {
                 curl_setopt($handle, CURLOPT_POST, TRUE);
                 curl_setopt($handle, CURLOPT_POSTFIELDS, $x->postData);
             }
+
+            curl_setopt($handle, CURLOPT_HEADER, TRUE);
+            curl_setopt($handle, CURLOPT_TIMEOUT, 45);//!!!?
+
             curl_multi_add_handle($multiHandle, $handle);
 
             $x->multiHandle = $multiHandle;
@@ -94,20 +101,51 @@ class MultiGet {
 
     public function request($url, $postFields = null, $curlOptions = null) {
         $postData = null;
-        if (isset($postFields)) {
+        if (isset($postFields) && is_array($postFields)) {
             $postData = '';
             foreach ($postFields as $k => $v) {
                 $postData .= ($postData !== '' ? '&' : '');
                 $postData .= urlencode($k) . '=' . urlencode($v);
             }
+        } else {
+          $postData = $postFields;
         }
         $x = new MultiGetRequest($url, $postData, $curlOptions);
         $this->_request($x);
         return $x;
     }
+    
+    public function setTimeout($callback, $delay) {
+        $this->timeouts[] = array(
+            'callback' => $callback,
+            'delay' => $delay,
+            'from' => microtime(true)
+        );
+    }
 
     public function go() {
-        while (count($this->requests) > 0) {
+        while (count($this->requests) > 0 || count($this->timeouts) > 0) {
+        
+       //echo count($this->requests) . ' / ' . count($this->timeouts) . " - r/t \n";
+            // check timeouts...
+            $i = 0;
+            $ts = microtime(true);
+            while ($i < count($this->timeouts)) {
+              $t = $this->timeouts[$i];
+              if ($t['from'] + $t['delay'] < $ts) {
+                $t = $this->timeouts[$i];
+                array_splice($this->timeouts, $i, 1);
+
+                $args = array();//?
+                call_user_func_array($t['callback'], $args);
+                
+                usleep(1000);
+                continue 2; //!
+              }
+              $i++;
+            }
+            //...
+
             $active = 1;
             while ($active > 0) {
                 $i = count($this->requests);
@@ -117,11 +155,20 @@ class MultiGet {
                 }
                 if ($active > 0) {
                     usleep(1000);
+                    continue 2; //!
                 }
             }
 
             $completed = $this->requests[$i];
             $content = curl_multi_getcontent($completed->handle);
+            
+            // http://stackoverflow.com/questions/4017911/curl-and-redirects-returning-multiple-headers
+            $curlInfo = curl_getinfo($completed->handle);
+            $headerSize = $curlInfo["header_size"];
+            $responseHeaders = substr($content, 0, $headerSize);
+            $content = substr($content, $headerSize);
+            //?
+
             curl_multi_remove_handle($completed->multiHandle, $completed->handle);
             curl_multi_close($completed->multiHandle);
 
@@ -138,7 +185,7 @@ class MultiGet {
             } else {
                 $completed->emit('success', $content, $completed->url, $completed->handle);          
             }
-            $completed->emit('complete', $content, $completed->url, $completed->handle);
+            $completed->emit('complete', $content, $completed->url, $completed->handle, $responseHeaders);
 
             curl_close($completed->handle);
         }
